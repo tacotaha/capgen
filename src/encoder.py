0a7443919a4ca370f18213be95b20ee959ba9779
Taha Azzaoui <taha@tahaazzaoui.com>
2019-03-23 23:28:58 -0400

    Updated encoder

--- src/encoder.py
+++ src/encoder.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python
-import json
+
 import numpy as np
 import tensorflow as tf
-from tf.keras.applications import InceptionV3
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications import inception_v3
 
 from dataset import Dataset
 
 class Encoder:
     def __init__(self, dataset):
-        assert isinstance(dataset, Datset)
+        assert isinstance(dataset, Dataset)
         self.dataset = dataset
         self.images = self.process_dataset()
         self.model = self.get_model()
 
     def process_dataset(self, batch_num=20):
-        fnames = sorted(set(self.dataset.keys()))
-        tf_dataset = tf.data.Datset.from_tensor_slices(fnames)
-        return tf_dataset.map(dataset.process_image).batch(batch_num)
+        fnames = sorted(set(self.dataset.cap_dict.keys()))
+        tf_dataset = tf.data.Dataset.from_tensor_slices(fnames)
+        return tf_dataset.map(self.process_image).batch(batch_num)
 
     def process_image(self,img_path):
         img = tf.read_file(img_path)
-        img = tf.image_decode_jpeg(img)
-        img = tf.image_resize(img, self.img_size)
-        img = tf.keras.inception_v3.preprocess_input(img)
-        return img, img_path
+        img = tf.image.decode_jpeg(img, channels=3)
+        img = tf.image.resize(img, self.dataset.img_size)
+        return inception_v3.preprocess_input(img), img_path
 
     def get_model(self, include_top=False, weights='imagenet'):
         img_model = InceptionV3(include_top=include_top, weights=weights)
-        return tf.keras.model(img_model.input, img_model.layers[-1].output)
+        return tf.keras.Model(img_model.input, img_model.layers[-1].output)
 
     def get_features(self):
         for x, y in self.images:
-            features = self.model(x)
-            features = tf.reshape(features, features.shape[0], -1, features.shape[3])
-            for f, p in zip(features, y):
-                np.save(p.numpy().decode(), f.numpy())
+            feats = self.model(x)
+            feats = tf.reshape(feats, (feats.shape[0], -1, feats.shape[3]))
+            for f, p in zip(feats, y): np.save(p.numpy().decode(), f.numpy())
