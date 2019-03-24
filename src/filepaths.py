c3e99b06047802b2b0aea2319455f6a4b7c6aece
Taha Azzaoui <taha@tahaazzaoui.com>
2019-03-23 21:42:28 -0400

    Added filepaths py

---
+++ src/filepaths.py
@@ -0,0 +1,8 @@
+import os
+
+"""
+Global data path definitions
+"""
+DATA_PATH = os.path.abspath("../data")
+TRAIN_IMG_DIR = os.path.join(DATA_PATH, "train2017")
+CAPTION_DIR = os.path.join(DATA_PATH, "annotations")
