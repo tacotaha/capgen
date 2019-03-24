d3f24d2fe82a136a70df9d357c0ecbd557b3987d
Taha Azzaoui <taha@tahaazzaoui.com>
2019-03-24 15:25:08 -0400

    Add existence check to save time

--- src/encoder.py
+++ src/encoder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.applications import InceptionV3
@@ -8,16 +9,17 @@ from tensorflow.keras.applications import inception_v3
 from dataset import Dataset
 
 class Encoder:
-    def __init__(self, dataset):
+    def __init__(self, dataset, batch_num=20):
         assert isinstance(dataset, Dataset)
         self.dataset = dataset
+        self.batch_num = batch_num
         self.images = self.process_dataset()
         self.model = self.get_model()
 
-    def process_dataset(self, batch_num=20):
+    def process_dataset(self):
         fnames = sorted(set(self.dataset.images))
         tf_dataset = tf.data.Dataset.from_tensor_slices(fnames)
-        return tf_dataset.map(self.process_image).batch(batch_num)
+        return tf_dataset.map(self.process_image).batch(self.batch_num)
 
     def process_image(self,img_path):
         img = tf.read_file(img_path)
@@ -33,4 +35,7 @@ class Encoder:
         for x, y in self.images:
             feats = self.model(x)
             feats = tf.reshape(feats, (feats.shape[0], -1, feats.shape[3]))
-            for f, p in zip(feats, y): np.save(p.numpy().decode(), f.numpy())
+            for f, p in zip(feats, y):
+                path = p.numpy().decode()
+                if not os.path.exists(path + ".npy"):
+                    np.save(path, f.numpy())
