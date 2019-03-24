c5be2857fbff39115bbb69dcaa5a1517d6e07a55
Taha Azzaoui <taha@tahaazzaoui.com>
2019-03-23 21:42:13 -0400

    Added datset class

---
+++ src/dataset.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from filepaths import *
+
+import json
+import sklearn
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tf.keras.applications import InceptionV3
+from sklearn.model_selection import train_test_split
+
+class Dataset:
+    def __init__(self, fname, img_size=(299, 299)):
+        self.fname = fname
+        self.img_size = img_size
+        self.cap_dict = self.read_captions()
+        self.model = self.get_model()
+
+    def read_captions(self):
+        """
+        fname: input filename containing annotations
+        returns: dict: img_file_name -> list(captions)
+        """
+        cap_dict = {}
+        with open(self.fname, "r") as f:
+            caps = json.load(f)
+
+        for cap in caps["annotations"]:
+            captions = cap["caption"]
+            img_id = cap["image_id"]
+            img_path = os.path.join(TRAIN_IMG_DIR, "%012d.jpg" % img_id)
+            cap_dict[img_path] = captions
+
+        return cap_dict
+
+if __name__ == "__main__":
+    fname = os.path.join(DATA_PATH, "annotations/captions_train2017.json")
+    training_data = Dataset(fname)
+    print(len(training_data.cap_dict))
