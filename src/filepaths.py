9c5c8e9ef0d430fcfe79caba40397bae14e08e99
Taha Azzaoui <taha@tahaazzaoui.com>
2019-04-11 00:07:12 -0400

    Added checkpoint directory to file paths

--- src/filepaths.py
+++ src/filepaths.py
@@ -3,6 +3,8 @@ import os
 """
 Global data path definitions
 """
+
 DATA_PATH = os.path.abspath("../data")
-TRAIN_IMG_DIR = os.path.join(DATA_PATH, "train2017")
+MODEL_DIR = os.path.abspath("../model")
 CAP_DIR = os.path.join(DATA_PATH, "annotations")
+TRAIN_IMG_DIR = os.path.join(DATA_PATH, "train2017")
