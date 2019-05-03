f0b17333cc02cd64a9a1cbc574db40325abad526
Taha Azzaoui <taha@tahaazzaoui.com>
2019-05-02 21:49:14 -0400

    Download captions

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -10,10 +10,15 @@ mkdir -p ../model
 mkdir -p ../data
 cd ../data
 
-echo -e "Downloading Images...\n"
+echo -e "Downloading Training Images...\n"
 wget http://images.cocodataset.org/zips/train2014.zip
 unzip train2017.zip 
 
 echo -e "Downloading Annotations...\n"
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 unzip annotations_trainval2014.zip
+
+
+echo -e "Downloading Validation Images...\n"
+wget http://images.cocodataset.org/zips/val2014.zip
+unzip val2014.zip
